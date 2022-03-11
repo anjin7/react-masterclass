6a0cff5f50210c15a7b230485a63b63ab377d3a7
_An_Jin_ <aji0719@gmail.com>
2022-03-11 14:11:01 +0900

    3.3

--- react-class/src/Circle.tsx
+++ react-class/src/Circle.tsx
@@ -2,27 +2,44 @@ import styled from 'styled-components';
 
 interface ContainerProps {
   bgColor: string;
+  borderColor?: string;
 };
 const Container = styled.div<ContainerProps>`
   width: 200px;
   height: 200px;
   background-color: ${props => props.bgColor};
+  border: 2px solid;
+  border-color: ${props => props.borderColor};
   border-radius: 100px;
+  text-align: center;
+  font-weight: 700;
+  font-size: 20px;
+  font-family: 'Times New Roman', Times, serif;
+  color: white;
 `;
+interface CircleProps {
+  bgColor: string;
+  borderColor?: string;
+  text?: string;
+}
 
 
-function Circle({ bgColor }: ContainerProps) {
-  return <Container bgColor={bgColor} />
+function Circle({ bgColor, borderColor, text = "default text" }: CircleProps) {
+  return (
+    <Container bgColor={bgColor} borderColor={borderColor ?? bgColor} >
+      {text}
+    </Container>
+  )
 };
 export default Circle;
 
-interface PlayerShape {
-  name: string;
-  age: number;
-}
+// interface PlayerShape {
+//   name: string;
+//   age: number;
+// }
 
-const sayHello = (playerObj: PlayerShape) =>
-  `Hello ${playerObj.name} you are ${playerObj.age} years old.`;
+// const sayHello = (playerObj: PlayerShape) =>
+//   `Hello ${playerObj.name} you are ${playerObj.age} years old.`;
 
-sayHello({ name: 'nico', age: 12 })
-sayHello({ name:'hi', age:12 })
+// sayHello({ name: 'nico', age: 12 })
+// sayHello({ name:'hi', age:12 })
