429942415ab776dedecef8cfde0a92524440d483
_An_Jin_ <aji0719@gmail.com>
2022-03-12 21:38:02 +0900

    3.6

--- react-class/src/App.tsx
+++ react-class/src/App.tsx
@@ -1,24 +1,25 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  background-color: ${props => props.theme.bgColor};
+  width: 300px;
+  height: 300px;
+  text-align: center;
+`;
+const H1 = styled.h1`
+  font-size: 32px;
+  font-weight: 700;
+  line-height: 100px;
+  color: ${props => props.theme.textColor};
+`;
 
 function App() {
-  const [value, setValue] = useState("");
-  const onChnage = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setValue(value);
-  };
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event?.preventDefault();
-    console.log("Hello", value)
-  };
+  
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input value={value} onChange={onChnage} type="text" placeholder='username' />
-        <button>Log In</button>
-      </form>
-    </div>  
+    <Container>
+      <H1>Hello</H1>
+    </Container>  
   );
 }
 
