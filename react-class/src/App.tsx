823227ab0a1d22c2a703517b2c3276f7d2b7d295
_An_Jin_ <aji0719@gmail.com>
2022-03-14 23:12:02 +0900

    4.0

--- react-class/src/App.tsx
+++ react-class/src/App.tsx
@@ -1,24 +1,10 @@
-import styled from 'styled-components';
-
-const Container = styled.div`
-  background-color: ${props => props.theme.bgColor};
-  width: 300px;
-  height: 300px;
-  text-align: center;
-`;
-const H1 = styled.h1`
-  font-size: 32px;
-  font-weight: 700;
-  line-height: 100px;
-  color: ${props => props.theme.textColor};
-`;
+// import styled from 'styled-components';
+import Router from './Router';
 
 function App() {
   
   return (
-    <Container>
-      <H1>Hello</H1>
-    </Container>  
+    <Router />
   );
 }
 
--- react-class/src/index.tsx
+++ react-class/src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { ThemeProvider } from 'styled-components';
 import App from './App';
-import './reset.css';
 import { lightTheme, darkTheme } from './theme';
 
 ReactDOM.render(
